e1d4de6fd63314593ed2ca9dec6e20130bbcbe67
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-02-25 21:26:03 +0530

    vercel

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,request
+from deepface import DeepFace
+import base64
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message":"Hello Flask"})
+
+@app.route('/emotion',methods=['POST'])
+def emotionDetection():
+    image=request.json['image']
+    image=image[22:]
+    decoded_image=base64.b64decode(image)
+
+    with open("sample.png","wb") as out_file:
+        out_file.write(decoded_image)
+
+    print("model time")
+    result=DeepFace.analyze('sample.png',actions=['emotion'])
+    output=result[0]['dominant_emotion']
+    return jsonify({"output":output})
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host="0.0.0.0",port=5000)
